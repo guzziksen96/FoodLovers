97ab38a7ac1538dac250315127747b447570060b
Klaudia <klaudia.guzik@codibly.com>
2019-10-06 22:25:01 +0200

    first commit

---
+++ FoodLovers/Configuration/IoConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FoodLovers.Api.Configuration
+{
+    public static class IoConfig
+    {
+        public static IServiceCollection RegisterIoDependecies(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //services.AddScoped<IService, Service>();
+            return services;
+        }
+    }
+}
